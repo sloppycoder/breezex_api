f7598a53fc526255a323230324e2565dacc4e63d
Li Lin <guru.lin@gmail.com>
2017-01-24 02:22:10 -0800

    align params usage with new json format.

--- app/controllers/device_registration_controller.rb
+++ app/controllers/device_registration_controller.rb
@@ -4,14 +4,15 @@ class DeviceRegistrationController < ApplicationController
   def create
     logger.info "received registration request from #{device_info(params)}"
 
-    device_id = params['device_uuid']
+    device_id = params['device_registration']['device_uuid']
     reg = DeviceRegistration.find_or_create_by(device_uuid: device_id)
     reg.user = current_user if reg.new_record?
     reg.notified = false
     reg.received = Time.now
     reg.save
 
-    notify_device(params['token'])
+    byebug
+    notify_device(params['device_registration']['token'])
 
     head :created
   end
@@ -22,9 +23,5 @@ class DeviceRegistrationController < ApplicationController
       event = Event.create(token: device_token)
       PushNotificationJob.perform_later event
     end
-  end
-
-  def device_info(params)
-    "Device #{params['model']}, #{params['device_id']}, #{params['device_uuid']}"
   end
 end
