e8ac3cd45b54d54b23e5b195b3a7fec02959bc34
Li Lin <guru.lin@gmail.com>
2017-01-30 01:40:07 -0800

    added casa_account and credt_card to root schema

--- app/graph/types/query_type.rb
+++ app/graph/types/query_type.rb
@@ -1,13 +1,29 @@
+# rubocop:disable Lint/UselessAssignment
 QueryType = GraphQL::ObjectType.define do
-  name "Query"
-  description "The query root for this schema"
- 
+  name 'Query'
+  description 'The query root for this schema'
+
   field :customer do
     type CustomerType
     argument :id, !types.ID
-    resolve -> (obj, args, ctx) {
+    resolve = lambda do |_obj, args, _ctx|
       Customer.find(args[:id])
-    }
+    end
+  end
+
+  field :casa_account do
+    type CasaAccountType
+    argument :id, !types.ID
+    resolve = lambda do |_obj, args, _ctx|
+      CasaAccount.find(args[:id])
+    end
+  end
+
+  field :credit_card do
+    type CreditCardType
+    argument :id, !types.ID
+    resolve = lambda do |_obj, args, _ctx|
+      CreditCard.find(args[:id])
+    end
   end
 end
- 
